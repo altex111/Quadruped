1bf02a521609f7e47e382ae95fc393ab4539ea23
altex111 <sanyi.komaromi@gmail.com>
2022-05-26 01:04:10 +0200

    minor update on ROS navigation

--- Software/Firmware/Quadroped_ros/Src/ros/ros_wrapper.cpp
+++ Software/Firmware/Quadroped_ros/Src/ros/ros_wrapper.cpp
@@ -202,7 +202,7 @@ void ROSNode::walk_inCommandCallback(const geometry_msgs::Pose& pose_msg)
 
 	//Get position from pose
 	walk_Command.x = pose_msg.position.x;
-	walk_Command.y = pose_msg.position.y;
+	walk_Command.z = pose_msg.position.z;
 
 	//Get heading from quat
 	walk_Command.phi = acos(pose_msg.orientation.x) * 2.0f;
